4cc6d12226de1c559d41f52a31f97eced1f4912b
Luis Donis <ldonis.emc2@gmail.com>
2023-10-07 19:12:02 -0600

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     mount Lesli::Engine => "/"
+    mount LesliDriver::Engine => "/driver" if defined?(LesliDriver)
     mount LesliAdmin::Engine => "/admin" if defined?(LesliAdmin)
+    mount LesliAudit::Engine => "/audit" if defined?(LesliAudit)
     mount LesliVault::Engine => "/vault" if defined?(LesliVault)
     mount LesliBabel::Engine => "/babel" if defined?(LesliBabel)
     mount LesliBell::Engine => "/bell" if defined?(LesliBell)
