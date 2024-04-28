19b501435f5015d0f302ab15413ebbb86cdbb8ab
Luis Donis <ldonis.emc2@gmail.com>
2024-04-27 20:42:25 -0600

    feat: improve welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 
 Rails.application.routes.draw do
-  resource :welcome
+  resource :welcome, only: [:show]
   root to: "welcomes#show"
 
   mount Lesli::Engine => "/lesli" if defined?(Lesli)
-  mount LesliDriver::Engine => "/driver" if defined?(LesliDriver)
-  mount LesliLetter::Engine => "/letter" if defined?(LesliLetter)
+  mount LesliBell::Engine => "/bell" if defined?(LesliBell)
   mount LesliAdmin::Engine => "/admin" if defined?(LesliAdmin)
   mount LesliAudit::Engine => "/audit" if defined?(LesliAudit)
-  mount LesliGuard::Engine => "/guard" if defined?(LesliGuard)
   mount LesliBabel::Engine => "/babel" if defined?(LesliBabel)
-  mount LesliBell::Engine => "/bell" if defined?(LesliBell)
+  mount LesliShield::Engine => "/shield" if defined?(LesliShield)
   mount LesliSupport::Engine => "/support" if defined?(LesliSupport)
+  mount LesliSecurity::Engine => "/security" if defined?(LesliSecurity)
+  mount LesliCalendar::Engine => "/calendar" if defined?(LesliCalendar)
   mount LesliDashboard::Engine => "/dashboard" if defined?(LesliDashboard)
 end
