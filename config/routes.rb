003b634f8e8d3daf7098d6b06b383814c2b85a9b
Luis Donis <ldonis.emc2@gmail.com>
2024-12-07 21:28:18 -0600

    use lesli routes constructor

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 
-# Mount login at root
-LesliShield::Routing.mount_login if defined?(LesliShield)
-
 Rails.application.routes.draw do
   resource :welcome, only: [:show]
   root to: "welcomes#show"
 
-  mount Lesli::Engine => "/lesli" if defined?(Lesli)
-  mount LesliBell::Engine => "/bell" if defined?(LesliBell)
-  mount LesliAdmin::Engine => "/admin" if defined?(LesliAdmin)
-  mount LesliAudit::Engine => "/audit" if defined?(LesliAudit)
-  mount LesliBabel::Engine => "/babel" if defined?(LesliBabel)
-  mount LesliShield::Engine => "/shield" if defined?(LesliShield)
-  mount LesliSupport::Engine => "/support" if defined?(LesliSupport)
-  mount LesliSecurity::Engine => "/security" if defined?(LesliSecurity)
-  mount LesliCalendar::Engine => "/calendar" if defined?(LesliCalendar)
-  mount LesliDashboard::Engine => "/dashboard" if defined?(LesliDashboard)
+  # Mount login at root
+  Lesli::Routing.login
+  Lesli::Routing.standard_engines
 end
