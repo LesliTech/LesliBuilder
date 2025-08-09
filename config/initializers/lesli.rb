87c66cbc728a64b9b15f8a9aa974a7b9184d8250
Luis Donis <ldonis.emc2@gmail.com>
2025-08-09 10:56:52 -0600

    update defaults

--- config/initializers/lesli.rb
+++ config/initializers/lesli.rb
@@ -3,5 +3,10 @@ if defined? Lesli
     Lesli.configure do |config|
         config.demo = true
         config.security[:allow_multiaccount] = false
+        config.locales = {
+            :en => "English",       # English
+            :es => "Español",       # Español/Spanish
+            :it => "Italiano",      # Italiano/Italian
+        }
     end
 end 
